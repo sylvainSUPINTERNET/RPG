ea5d11101ac5482a5ba043df79f2c2a327bc3392
JOLY Sylvain <jolysylvain@MPEC-PDT-33.local>
2018-10-09 12:45:46 +0200

    fixed board texture

--- src/Character.php
+++ src/Character.php
@@ -9,4 +9,49 @@
 class Character
 {
 
+    protected $username;
+    protected $class;
+
+    public function __construct()
+    {
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getClass()
+    {
+        return $this->class;
+    }
+
+    /**
+     * @param mixed $class
+     */
+    public function setClass($class)
+    {
+        $this->class = $class;
+    }
+
+
+
+
+
+
+
 }
