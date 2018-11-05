9b9145357720e308c5ece5ee387b2926975cc956
JOLY Sylvain <jolysylvain@MPEC-PDT-33.local>
2018-11-05 09:10:28 +0100

    style

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,8 +4,15 @@
 let RPG = angular.module('RPG', [])
 
 .controller("mapController", function($scope) {
+
+    $scope.character = {
+        map_position: 1 // start à 1
+    };
+
+
     $scope.helloTo = {};
     $scope.helloTo.title = "AngularJS";
+
 
     $scope.player = {
         character_class : "",
